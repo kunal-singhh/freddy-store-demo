d050451d7acf24f45541ffd29999db4ec74aaf28
kunal-singhh <kunal.singh@freshworks.com>
2023-05-12 12:30:41 +0530

    Starting Point

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,25 +8,5 @@ async function init() {
 }
 
 async function renderText() {
-  const products = await fetchProductsFromFakeStore(client);
-  document.body.innerHTML = JSON.stringify(products);
+  document.body.innerHTML = "Hello World";
 }
-
-// Function to call the FakeStore API products endpoint and return the result object using fdk request method template
-  async function fetchProductsFromFakeStore(client) {
-    try {
-      const response = await client.request.invokeTemplate("fakeStoreGetProducts", {
-        cache: true,
-        ttl: 70000
-      });
-      if (response.status === 200) {
-        const products = response.response;
-        return JSON.parse(products);
-      } else {
-        throw new Error(`Failed to fetch products: ${response.status} ${response.statusText}`);
-      }
-    } catch (error) {
-      console.error("Error fetching products from the FakeStore API:", error);
-      throw error;
-    }
-  }
